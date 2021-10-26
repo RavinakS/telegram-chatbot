91bae11b97019ba62051d69bd09d0cb269de49de
RavinakS <ravinasingh697@gmail.com>
2021-10-27 02:04:02 +0530

    dotenv file

--- connection.js
+++ connection.js
@@ -1,6 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
+require('dotenv').config();
 
-const token = "2050431990:AAEWD2fihGVg2ElaDJg6A88bWEwg6f5hjEs";
+const token = process.env.token
 
 const bot = new TelegramBot(token, {polling: true});
 
